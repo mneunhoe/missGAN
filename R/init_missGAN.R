f6286ce2b0f6d65bd6b90ae2e2053fb7e8f41a82
mneunhoe <mneunhoe@mail.uni-mannheim.de>
2021-01-14 17:11:47 +0100

    started work on missGAN

--- R/init_missGAN.R
+++ R/init_missGAN.R
@@ -329,7 +329,7 @@ GAN2_update_step <-
     }
 
     z_enc <- GAN_nets$encoder(real_data*real_mask)
-    z_gen <- torch::torch_empty_like(z_enc)$normal_()$to(device =device)
+    z_gen <- torch::torch_empty_like(z_enc)$cpu()$normal_()$to(device = device)
 
     x_gen <- apply_activate(GAN_nets$decoder(z_gen), GAN_nets$transformer)
     x_rec <- apply_activate(GAN_nets$decoder(z_enc), GAN_nets$transformer)
@@ -381,7 +381,7 @@ GAN2_update_step <-
     # To update the Generator we will use a fresh noise sample.
     # torch_randn creates a torch object filled with draws from a standard normal distribution
     z_enc <- GAN_nets$encoder(real_mask*real_data)
-    z_gen <- torch::torch_empty_like(z_enc)$normal_()$to(device = device)
+    z_gen <- torch::torch_empty_like(z_enc)$cpu()$normal_()$to(device = device)
 
     x_gen <- apply_activate(GAN_nets$decoder(z_gen), GAN_nets$transformer)
     x_rec <- apply_activate(GAN_nets$decoder(z_enc), GAN_nets$transformer)
