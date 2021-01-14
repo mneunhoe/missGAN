1eda17204db396a21cfe5827935166708d7cf888
mneunhoe <mneunhoe@mail.uni-mannheim.de>
2021-01-14 17:46:36 +0100

    started work on missGAN

--- R/init_missGAN.R
+++ R/init_missGAN.R
@@ -193,7 +193,7 @@ GAN_update_step <-
 
 init_weights <- function(m){
   if( "nn_linear" %in% attributes(m)$class){
-    torch::nn_init_kaiming_normal_(m$weight$cpu()$to(device = device))
+    torch::nn_init_kaiming_normal_(m$weight$cpu())$to(device = device)
     m$bias$data()$cpu()$fill_(0.01)$to(device = device)
   }
 
