22b3aeda83ef2774b8a265debbd995511665645c
Marcel Neunhoeffer <mneunhoe@mail.uni-mannheim.de>
2021-04-05 15:04:04 +0200

    Update GAN_functions.R

--- R/GAN_functions.R
+++ R/GAN_functions.R
@@ -220,6 +220,53 @@ Discriminator <- torch::nn_module(
   }
 )
 
+Encoder <- torch::nn_module(
+  initialize = function(noise_dim,
+                        # The length of our noise vector per example
+                        data_dim,
+                        # The number of columns in our data
+                        hidden_units = list(128, 128),
+                        # A list with the number of neurons per layer. If you add more elements to the list you create a deeper network.
+                        dropout_rate = 0.5 # The dropout probability
+                        )
+                        {
+                        # Initialize an empty nn_sequential module
+                        self$seq <- torch::nn_sequential()
+
+                        # For the hidden layers we need to keep track of our input and output dimensions. The first input will be our noise vector, therefore, it will be noise_dim
+                        dim <- noise_dim
+
+                        # i will be a simple counter to keep track of our network depth
+                        i <- 1
+
+                        # Now we loop over the list of hidden units and add the hidden layers to the nn_sequential module
+                        for (neurons in hidden_units) {
+                          self$seq$add_module(module = torch::nn_linear(dim, neurons),
+                                              name = paste0("Linear_", i))
+                          # Add a leakyReLU activation
+                          self$seq$add_module(module = torch::nn_leaky_relu(),
+                                              name = paste0("Activation_", i))
+                          # And a Dropout layer
+                          self$seq$add_module(module = torch::nn_dropout(dropout_rate),
+                                              name = paste0("Dropout_", i))
+
+                          dim <- neurons
+                          # Update the counter
+                          i <- i + 1
+                        }
+                        # Finally, we add the output layer. The output dimension must be the same as our data dimension (data_dim).
+                        # self$seq$add_module(module = nn_linear(dim, data_dim),
+                        #                     name = "Output")
+                        self$enc_mu <- torch::nn_linear(dim, data_dim)
+                        self$enc_logvar <- torch::nn_linear(dim, data_dim)
+                        },
+forward = function(input) {
+  output <- self$seq(input)
+  mu <- self$enc_mu(output)
+  logvar <- self$enc_logvar(output)
+  return(list(mu, logvar))
+}
+  )
 
 SpectralNorm <- torch::nn_module(
   initialize = function(module, name = "weight", power_iterations = 1) {
